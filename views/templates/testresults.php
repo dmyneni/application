a378ca1a29cd3a55a694d63350fda2b651a2a667
dmyneni <cdbhavani@yahoo.com>
2016-10-14 20:43:28 -0500

    Add files via upload

--- views/templates/testresults.php
+++ views/templates/testresults.php
@@ -1,6 +1,35 @@
-<div class="container" >
-        <h3 style="font-size:15pt">Query Results</h1>
-        <br />
+<div id="main" role="main">
+<section id="widget-grid" class="">
+
+
+			<!-- START ROW -->
+
+			<div class="row">
+
+				<!-- NEW COL START -->
+					<!-- NEW COL START -->
+				<article class="col-sm-8 col-md-12 col-lg-10">
+					
+					<!-- Widget ID (each widget will need unique ID) registrtion form-->
+					<div class="jarviswidget" id="wid-id-4" data-widget-editbutton="false" data-widget-custombutton="false">
+						<header>
+<!--							<span class="widget-icon"> <i class="fa fa-edit"></i> </span> -->
+							<h2>Query Results</h2>				
+							
+						</header>
+
+						<!-- widget div-->
+						<div>
+							
+							<!-- widget edit box -->
+							<div class="jarviswidget-editbox">
+								<!-- This area used as dropdown edit box -->
+								
+							</div>
+							<!-- end widget edit box -->
+							
+							<!-- widget content -->
+							<div class="widget-body no-padding">
         <table id="table" class="table table-striped table-bordered">
             <thead>
                 <tr>
@@ -27,4 +56,25 @@
                      <?php }?>
             </tbody>
         </table>
-    </div>
+    						</div>
+							<!-- end widget content -->
+							
+						</div>
+						<!-- end widget div -->
+						
+					</div>
+					<!-- end widget -->
+			
+
+				</article>
+				<!-- END COL -->		
+
+			</div>
+
+			<!-- END ROW -->
+
+		</section>
+		<!-- end widget grid -->
+
+</div>
+    
